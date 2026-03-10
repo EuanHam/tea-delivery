209d1768a7841c1e8f756150447995de9d179c89
Park, Brian <bpark306@gatech.edu>
2026-03-10 14:21:34 -0400

    Added Drag to the movement

--- Assets/Scripts/RobbiController.cs
+++ Assets/Scripts/RobbiController.cs
@@ -51,16 +51,34 @@ public class RobbiController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Forward Speed
         if (speed > 0f && sphere.linearVelocity.magnitude < topSpeed)
             sphere.AddForce(transform.forward * speed * acceleration, ForceMode.Acceleration);
 
+        // Reverse Speed
         else if (speed < 0f && sphere.linearVelocity.magnitude < reverseSpeed)
             sphere.AddForce(transform.forward * speed * reverseAcceleration, ForceMode.Acceleration);
 
+        // Adding gravity
         sphere.AddForce(Vector3.down * gravity , ForceMode.Acceleration);
 
+        // Adding Rotation
         Quaternion rotation = Quaternion.Euler(0f, rotate * turnSpeed * Time.fixedDeltaTime, 0f);
         transform.rotation *= rotation;
 
+        // Adding Drag
+        Vector3 localVel = transform.InverseTransformDirection(sphere.linearVelocity);
+
+        // reduce sideways sliding
+        localVel.x *= drag;
+
+        // optional: small forward drag when not accelerating
+        if (Mathf.Abs(speed) < 0.1f)
+        {
+            localVel.z *= 0.98f;
+        }
+
+        sphere.linearVelocity = transform.TransformDirection(localVel);
+                
     }
 }
