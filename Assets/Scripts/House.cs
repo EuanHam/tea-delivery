f2f5512204d92fa6f3ff9233c8186facd2858404
Hebe3834 <hebeluna00@gmail.com>
2026-03-13 03:17:46 -0400

    generates random orders and shows them in UI, also integrated random orders into gameplay

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -2,11 +2,34 @@ using UnityEngine;
 
 public class House : MonoBehaviour
 {
+    BobaDeliveryManager bobaDeliveryManager = BobaDeliveryManager.Instance;
+    
+    void Start()
+    {
+        bobaDeliveryManager = BobaDeliveryManager.Instance;
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            BobaDeliveryManager.Instance.DeliverBoba();
+            if (bobaDeliveryManager.hasBoba == false)
+            {
+                bobaDeliveryManager.UpdateHUD("You don't have a boba to deliver!");
+                return;
+            }
+            if (bobaDeliveryManager.currentOrder != null)
+            {
+                Transform deliveryLocation = bobaDeliveryManager.currentOrder.deliveryLocation;
+                if (Vector3.Distance(transform.position, deliveryLocation.position) > 2f)
+                {
+                    bobaDeliveryManager.UpdateHUD("This isn't the right house!");
+                    return;
+                } else
+                {
+                    bobaDeliveryManager.DeliverBoba();
+                    return;
+                }
+            }
         }
     }
 }
---
+++ Assets/Scripts/Types/BobaType.cs
@@ -0,0 +1,6 @@
+public enum Boba
+{
+    MilkTea,
+    Matcha,
+    Taro
+}
