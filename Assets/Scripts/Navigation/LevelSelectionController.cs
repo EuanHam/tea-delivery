a5101177caa3e5706f2e3b671b4f195e10203f65
EuanHam <euanham05@gmail.com>
2026-03-15 22:43:28 -0400

    Fix tag issue and add audio

--- Assets/Scripts/Navigation/LevelSelectionController.cs
+++ Assets/Scripts/Navigation/LevelSelectionController.cs
@@ -8,11 +8,14 @@ public class LevelSelectionController : MonoBehaviour
     [SerializeField] private Button backButton;
     [SerializeField] private Button[] levelButtons;
     
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    
     void Start()
     {
         if (backButton != null)
         {
-            backButton.onClick.AddListener(GoToTitleScreen);
+            backButton.onClick.AddListener(() => PlaySoundAndGoToTitleScreen());
         }
         else
         {
@@ -21,16 +24,40 @@ public class LevelSelectionController : MonoBehaviour
         
         if (levelButtons != null && levelButtons.Length >= 4)
         {
-            levelButtons[0].onClick.AddListener(() => LoadLevel("Level0Tutorial"));
+            levelButtons[0].onClick.AddListener(() => PlaySoundThenLoadLevel("Level0Tutorial"));
             
             // levels 1-3 (later)
-            levelButtons[1].onClick.AddListener(() => LoadLevel("Level1"));
-            levelButtons[2].onClick.AddListener(() => LoadLevel("Level2"));
-            levelButtons[3].onClick.AddListener(() => LoadLevel("Level3"));
+            levelButtons[1].onClick.AddListener(() => PlaySoundThenLoadLevel("Level1"));
+            levelButtons[2].onClick.AddListener(() => PlaySoundThenLoadLevel("Level2"));
+            levelButtons[3].onClick.AddListener(() => PlaySoundThenLoadLevel("Level3"));
         }
         else
         {
             Debug.LogError("Check the number of buttons assigned!");
+        }
+    }
+    
+    void PlaySoundAndGoToTitleScreen()
+    {
+        PlaySound();
+        GoToTitleScreen();
+    }
+    
+    void PlaySoundThenLoadLevel(string levelName)
+    {
+        PlaySound();
+        LoadLevel(levelName);
+    }
+    
+    void PlaySound()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("AudioSource not assigned!");
         }
     }
     
