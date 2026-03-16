3c699f1ee8aa7d803ce56642fe564acb75b36bdd
Isiah Julian Nogal <isiahjnogal@gmail.com>
2026-03-16 00:26:00 -0400

    fixed button sounds for title screen and level selection screen

--- Assets/Scripts/Navigation/LevelSelectionController.cs
+++ Assets/Scripts/Navigation/LevelSelectionController.cs
@@ -1,55 +1,58 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 
 public class LevelSelectionController : MonoBehaviour
 {
     [Header("UI References")]
     [SerializeField] private Button backButton;
     [SerializeField] private Button[] levelButtons;
-    
+
     [Header("Audio")]
     [SerializeField] private AudioSource audioSource;
-    
+
     void Start()
     {
         if (backButton != null)
         {
-            backButton.onClick.AddListener(() => PlaySoundAndGoToTitleScreen());
+            backButton.onClick.AddListener(() => StartCoroutine(PlaySoundAndGoToTitleScreen()));
         }
         else
         {
             Debug.LogError("Configure back button on LevelSelectionController");
         }
-        
+
         if (levelButtons != null && levelButtons.Length >= 4)
         {
-            levelButtons[0].onClick.AddListener(() => PlaySoundThenLoadLevel("Level0Tutorial"));
-            
-            // levels 1-3 (later)
-            levelButtons[1].onClick.AddListener(() => PlaySoundThenLoadLevel("Level1"));
-            levelButtons[2].onClick.AddListener(() => PlaySoundThenLoadLevel("Level2"));
-            levelButtons[3].onClick.AddListener(() => PlaySoundThenLoadLevel("Level3"));
+            levelButtons[0].onClick.AddListener(() => StartCoroutine(PlaySoundThenLoadLevel("Level0Tutorial")));
+            levelButtons[1].onClick.AddListener(() => StartCoroutine(PlaySoundThenLoadLevel("Level1")));
+            levelButtons[2].onClick.AddListener(() => StartCoroutine(PlaySoundThenLoadLevel("Level2")));
+            levelButtons[3].onClick.AddListener(() => StartCoroutine(PlaySoundThenLoadLevel("Level3")));
         }
         else
         {
             Debug.LogError("Check the number of buttons assigned!");
         }
     }
-    
-    void PlaySoundAndGoToTitleScreen()
+
+    private IEnumerator PlaySoundAndGoToTitleScreen()
     {
         PlaySound();
-        GoToTitleScreen();
+        if (audioSource != null)
+            yield return new WaitForSeconds(audioSource.clip.length);
+        SceneManager.LoadScene("TitleScreen");
     }
-    
-    void PlaySoundThenLoadLevel(string levelName)
+
+    private IEnumerator PlaySoundThenLoadLevel(string levelName)
     {
         PlaySound();
-        LoadLevel(levelName);
+        if (audioSource != null)
+            yield return new WaitForSeconds(audioSource.clip.length);
+        SceneManager.LoadScene(levelName);
     }
-    
-    void PlaySound()
+
+    private void PlaySound()
     {
         if (audioSource != null)
         {
@@ -59,15 +62,5 @@ public class LevelSelectionController : MonoBehaviour
         {
             Debug.LogError("AudioSource not assigned!");
         }
-    }
-    
-    void GoToTitleScreen()
-    {
-        SceneManager.LoadScene("TitleScreen");
-    }
-    
-    void LoadLevel(string levelName)
-    {
-        SceneManager.LoadScene(levelName);
     }
 }
--- Assets/Scripts/Navigation/TitleScreenController.cs
+++ Assets/Scripts/Navigation/TitleScreenController.cs
@@ -1,26 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
 
 public class TitleScreenController : MonoBehaviour
 {
     [Header("UI References")]
     [SerializeField] private Button playButton;
-    
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+
     void Start()
     {
         if (playButton != null)
         {
-            playButton.onClick.AddListener(GoToLevelSelection);
+            playButton.onClick.AddListener(() => StartCoroutine(PlaySoundAndGoToLevelSelection()));
         }
         else
         {
             Debug.LogError("Play button not configured in TitleScreenController");
         }
     }
-    
-    void GoToLevelSelection()
+
+    private IEnumerator PlaySoundAndGoToLevelSelection()
     {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+            yield return new WaitForSeconds(audioSource.clip.length);
+        }
+
         SceneManager.LoadScene("LevelSelection");
     }
 }
