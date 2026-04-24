f23e30bfb1f744ea794857c06853493e41be8e92
Brian P <bpark4684@gmail.com>
2026-04-23 21:20:40 -0400

    Added Stunned Sound Effects

--- Assets/Scripts/Player/VehicleCollision.cs
+++ Assets/Scripts/Player/VehicleCollision.cs
@@ -8,6 +8,7 @@ public class VehicleCollision : MonoBehaviour
     [SerializeField] private PowerUpManager powerUpManager;
     [SerializeField] private BobaDriver bd;
     [SerializeField] private float stunDuration;
+    [SerializeField] private AudioClip coin_drop, dizzy_sound;
 
     private void OnCollisionEnter(Collision c)
     {
@@ -15,9 +16,14 @@ public class VehicleCollision : MonoBehaviour
         {
             Debug.Log("vehicle collision!");
             if (!powerUpManager.isInvunerable() && !robbiController.stunned) {
+                if (bd.balance > 0f)
+                {
+                    PlayCoinDrop();
+                }
                 bd.balance -= 100;
                 bd.balance = Mathf.Max(0, bd.balance);
                 bd.npcsHit += 1;
+                PlayDizzy();
                 StartCoroutine(stunnedMovement());
             }
         }
@@ -33,5 +39,21 @@ public class VehicleCollision : MonoBehaviour
     public bool isStunned ()
     {
         return robbiController.stunned;
+    }
+
+    public void PlayCoinDrop()
+    {
+        if (coin_drop != null)
+        {
+            AudioSource.PlayClipAtPoint(coin_drop, transform.position);
+        }
+    }
+
+        public void PlayDizzy()
+    {
+        if (coin_drop != null)
+        {
+            AudioSource.PlayClipAtPoint(dizzy_sound, transform.position);
+        }
     }
 }
