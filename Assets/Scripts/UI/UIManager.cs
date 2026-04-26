ce923ad9c798a3d08a084aa148083f0571225c56
Hebe3834 <hebeluna00@gmail.com>
2026-04-25 22:19:02 -0400

    instructions and lore

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using System;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public struct Dialogue
 {
@@ -16,11 +17,39 @@ public class UIManager : MonoBehaviour
 {
     private Dialogue[] tutorialDialogue = new Dialogue[]
     {
-        new Dialogue { speaker = "Robbi", text = "Welcome to the city, Robbi!" },
-        new Dialogue { speaker = "Robbi", text = "Say Hello to Robbi!\n(Press space)" },
-        new Dialogue { speaker = "Robbi", text = "Help Robbi find the Bubble Tea Shop!" },
-        new Dialogue { speaker = "Robbi", text = "Use WASD to move around and press SPACE to honk!" }
+        new Dialogue { speaker = "Mayor", text = "Welcome to the city, Robbi!" },
+        new Dialogue { speaker = "Mayor", text = "We at Bobaville are all so excited for your new boba shop!!!" },
+        new Dialogue { speaker = "Mayor", text = "Pick up the boba order from the shop and deliver it to the customer. Feel free to use your built in minimap on the bottom right!" },
+        new Dialogue { speaker = "Mayor", text = "Use WASD to move around \n Press SPACE to jump" },
+        new Dialogue { speaker = "Mayor", text = "And mind the time limit on the top of the screen" },
+        new Dialogue { speaker = "Mayor", text = "For your first delivery, the entire town has decided to support you. We will all stand still so we don't get in the way of your work. No pressure!" },
+        new Dialogue { speaker = "Robbi", text = "Beep boop ('''' •᷄ ᴗ •᷅ )" }
     };
+
+    private Dialogue[] level1Dialogue = new Dialogue[]
+    {
+        new Dialogue { speaker = "Mayor", text = "Congrats on your first order!" },
+        new Dialogue { speaker = "Mayor", text = "We have now unlocked the entire town for you and people will now move." },
+        new Dialogue { speaker = "Mayor", text = "Don't hit anyone because that is bad for business. You might also lose a bit of money." },
+        new Dialogue { speaker = "Mayor", text = "By the way, you can collect power-ups! They look like question marks and will help you on your journey." },
+        new Dialogue { speaker = "Mayor", text = "They can help you double your next earnings, get more time, or become invulnerable.  Good luck!" },
+        new Dialogue { speaker = "Robbi", text = "Beep boop (^-^ ) " }
+    };
+
+    private Dialogue[] level2Dialogue = new Dialogue[]
+    {
+        new Dialogue { speaker = "Mayor", text = "Yay! Looks like things are going well. I think you're ready for vehicles." },
+        new Dialogue { speaker = "Mayor", text = "If you collide with them you will lose a lot of money. And also be stunned. Good luck!" },
+        new Dialogue { speaker = "Robbi", text = "Beep boop (˵ •̀ ᴗ •́ ˵ )" }
+    };
+
+    private Dialogue[] level3Dialogue = new Dialogue[]
+    {
+        new Dialogue { speaker = "Mayor", text = "Excellent work! For your next challenge, the penalties will be even higher. Good luck!" },
+        new Dialogue { speaker = "Robbi", text = "Beep boop (ᵔuᵔ)" }
+    };
+
+    private Dialogue[] currentDialogue;
     private int instructionIndex;
 
     [SerializeField] private LevelManager lm;
@@ -60,6 +89,28 @@ public class UIManager : MonoBehaviour
         endUI.SetActive(false);
         doubleMoneyUI.SetActive(false);
         invulnerableUI.SetActive(false);
+
+
+        string levelName = SceneManager.GetActiveScene().name;
+
+        switch (levelName)
+        {
+            case "Level0Tutorial":
+                currentDialogue = tutorialDialogue;
+                break;
+            case "Level1":
+                currentDialogue = level1Dialogue;
+                break;
+            case "Level2":
+                currentDialogue = level2Dialogue;
+                break;
+            case "Level3":
+                currentDialogue = level3Dialogue;
+                break;
+            default:
+                currentDialogue = tutorialDialogue;
+                break;
+        }
 
 
         ShowInstruction(instructionIndex);
@@ -114,13 +165,15 @@ public class UIManager : MonoBehaviour
 
     private void ShowInstruction(int index)
     {
-        if (index >= tutorialDialogue.Length)
+        if (index >= currentDialogue.Length)
         {
             instructionUI.SetActive(false);
             return;
         }
+
+
         
-        Dialogue line = tutorialDialogue[index];
+        Dialogue line = currentDialogue[index];
         instructionText.text = line.speaker + ": " + line.text;
         // hintText.text = "(Press space to continue)";
     }
